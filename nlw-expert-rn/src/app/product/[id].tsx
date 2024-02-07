4e73d3048d2c103cee4b8d5cad96ac889a4793c2
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-02-07 00:43:18 -0300

    feat: add product screen

--- nlw-expert-rn/src/app/product/[id].tsx
+++ nlw-expert-rn/src/app/product/[id].tsx
@@ -1,12 +1,54 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { PRODUCTS } from "@/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
 
 export default function Product() {
   const { id } = useLocalSearchParams();
 
+  const product = PRODUCTS.find((item) => item.id === id)!;
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-white">{id}</Text>
+    <View className="flex-1">
+      <Image
+        className="w-full h-52"
+        source={product.cover}
+        resizeMode="cover"
+      />
+
+      <View className="flex-1 p-5 mt-8">
+        <Text className="text-white text-xl font-header">{product.title}</Text>
+        <Text className="text-lime-400 text-2xl font-header my-2">
+          {formatCurrency(product.price)}
+        </Text>
+
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+          {product.description}
+        </Text>
+
+        {product.ingredients.map((ingredient) => (
+          <Text
+            className="text-slate-400 font-body text-base leading-6"
+            key={ingredient}
+          >
+            {"\u2022"} {ingredient}
+          </Text>
+        ))}
+      </View>
+
+      <View className="p-5 pb-8 gap-5">
+        <Button>
+          <Button.Icon>
+            <Feather name="plus-circle" size={20} />
+          </Button.Icon>
+          <Button.Text>Adicionar ao pedido</Button.Text>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/"></LinkButton>
+      </View>
     </View>
   );
 }
